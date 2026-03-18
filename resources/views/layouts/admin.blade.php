c449485526a9f9813758824c9e2b645168c41d03
DfxGaming <107567446+DfxGaming@users.noreply.github.com>
2026-03-19 03:17:01 +0530

    mail env setup

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -44,7 +44,7 @@
             <svg class="w-5 h-5 text-white" viewBox="0 0 24 24" fill="currentColor"><path d="M7 2a2 2 0 00-2 2v16a2 2 0 002 2h10a2 2 0 002-2V4a2 2 0 00-2-2H7zm5 17a1 1 0 110-2 1 1 0 010 2z"/></svg>
         </div>
         <div>
-            <p class="font-black text-white text-sm leading-tight">MobileShop</p>
+            <p class="font-black text-white text-sm leading-tight">Harahori Telecom</p>
             <p class="text-gray-500 text-xs">Admin Panel</p>
         </div>
     </div>

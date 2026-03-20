853438acd6a710eed2d526c10a78df7ff1f9467b
DfxGaming <107567446+DfxGaming@users.noreply.github.com>
2026-03-20 13:38:16 +0530

    spectacles

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
     ])->toArray(),
 
     /*
